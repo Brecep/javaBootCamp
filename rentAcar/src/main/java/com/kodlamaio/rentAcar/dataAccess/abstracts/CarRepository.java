c6299024f2c3ad83dd1d955421a45930c8e39b0f
Recep Bayındır <95147143+Brecep@users.noreply.github.com>
2022-06-08 12:14:59 +0300

    Delete rentAcar directory
    deleted

--- rentAcar/src/main/java/com/kodlamaio/rentAcar/dataAccess/abstracts/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kodlamaio.rentAcar.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kodlamaio.rentAcar.entities.concretes.Car;
-
-public interface CarRepository extends JpaRepository<Car, Integer> {
-
-}
--- rentAcar/src/main/java/com/kodlamaio/rentAcar/dataAccess/abstracts/ColorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kodlamaio.rentAcar.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kodlamaio.rentAcar.entities.concretes.Color;
-
-public interface ColorRepository extends JpaRepository<Color, Integer>{
-
-}
