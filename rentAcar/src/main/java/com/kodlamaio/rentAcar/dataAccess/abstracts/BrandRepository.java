8fcb0fcbdb2dfb2b5d665d79a8366e25608bedc4
Recep Bayındır <95147143+Brecep@users.noreply.github.com>
2022-06-13 14:16:28 +0300

    deleted rentacar

--- rentAcar/src/main/java/com/kodlamaio/rentAcar/dataAccess/abstracts/BrandRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kodlamaio.rentAcar.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kodlamaio.rentAcar.entities.concretes.Brand;
-
-public interface BrandRepository extends JpaRepository<Brand, Integer>{
-	//Jpa ile crud işlemlerini kendisi hallediyor
-}
